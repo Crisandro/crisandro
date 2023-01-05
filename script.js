33be3f671cdce31eca76b36b71ac09f55de65ac2
Crisandro Basoy <crisandro.cloudemployee@gmail.com>
2023-01-06 03:25:43 +0800

    card finalization

--- script.js
+++ script.js
@@ -1,23 +1,43 @@
-const front = document.querySelector(".front");
 const card = document.querySelector("#card");
+const shadow = document.getElementById("shadowbox");
 const pic = document.querySelector("#photos");
-var starting = 0;
+let darkBoxVisible = false, starting = true;
 //card.vanillaTilt.destroy();
 
-front.addEventListener("click", cardmove);
+card.addEventListener("click", cardin);
 
-function cardmove(){
-    if(starting == 0){
-        card.style.zIndex = "5";
-        card.style.top = "10vh";
-        card.style.transform = "rotateY(0deg)";
-        starting = 1;
-    }
-    else{  
-        card.style.zIndex = "-5";
+shadow.addEventListener("click", cardout);
+
+function cardout(){
+    if(starting==false){
+        shadow.style.opacity = "0";
+        shadow.style.visibility = "hidden";
+        card.vanillaTilt.destroy();
         card.style.top = "75vh";
+        card.style.left = "2vw";
         card.style.transform = "rotateY(180deg)";
-        starting = 0;
+        starting = true;
+    }
+}
+
+function cardin(){
+    if(starting){
+        shadow.style.opacity = "1";
+        shadow.style.visibility = "visible";
+        card.style.top = "10vh";
+        card.style.left = "30vw";
+        card.style.transform = "rotateY(0deg)";
+        starting = false;
+        VanillaTilt.init(document.querySelector("#card"), {
+            max: 10,
+            speed: 1000,
+            glare: true,
+            startX: 0,
+            startY: 0,
+            "max-glare": 0.5,
+            scale: 1.1,
+            perspective: 1500
+        });
     }
 }
 
@@ -31,3 +51,47 @@ window.addEventListener("wheel", function (e) {
     if (e.deltaY > 0) pic.scrollLeft += 100;
     else pic.scrollLeft -= 100;
 });
+
+function shadowy(){
+    console.log("done");
+    
+}
+
+/*function showDarkbox(url) {
+    if(!darkBoxVisible) {
+     let x = (window.innerWidth - 1280) / 2;
+     let y = window.scrollY + 50;
+     // Create the darkBox
+     var div = document.createElement("div");
+     div.id = "darkbox";
+     div.innerHTML = '<img class="darkboximg" src="'+url+'" />';
+     document.body.appendChild(div);
+     let box = document.getElementById("darkbox");
+     box.style.left = x.toString()+"px";
+     box.style.top = y.toString()+"px";
+     box.style.height = 'auto';
+     box.addEventListener('click', (event) => {
+      // Remove it
+      let element = document.getElementById("darkbox");
+      element.parentNode.removeChild(element);
+      darkBoxVisible = false;
+     })
+     darkBoxVisible = true;
+   } else {
+     // Remove it
+     let element = document.getElementById("darkbox");
+     element.parentNode.removeChild(element);
+     darkBoxVisible = false;
+  }
+ }
+
+ window.addEventListener('load', (event) => {
+    let images = document.querySelectorAll("images");
+    if(images !== null && images !== undefined && images.length > 0) {
+     images.forEach(function(img) {
+      img.addEventListener('click', (evt) => {
+       showDarkbox(img.src);
+      });
+     });
+    }
+});*/
