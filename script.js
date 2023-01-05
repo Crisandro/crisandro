18b5c80ca2ce3fa726282d9a6341257fb96a2376
Crisandro Basoy <crisandro.cloudemployee@gmail.com>
2023-01-06 01:44:54 +0800

    adding additional images

--- script.js
+++ script.js
@@ -1,14 +1,33 @@
 const front = document.querySelector(".front");
 const card = document.querySelector("#card");
+const pic = document.querySelector("#photos");
+var starting = 0;
+//card.vanillaTilt.destroy();
 
-card.addEventListener("click", function() {    
-    console.log(card.style.top == "75vh");
-    if(card.style.top == "10vh"){
-        card.style.top = "75vh";
-        card.style.transform = "rotateY(180deg)";
-    }
-    else{
+front.addEventListener("click", cardmove);
+
+function cardmove(){
+    if(starting == 0){
+        card.style.zIndex = "5";
         card.style.top = "10vh";
         card.style.transform = "rotateY(0deg)";
+        starting = 1;
     }
+    else{  
+        card.style.zIndex = "-5";
+        card.style.top = "75vh";
+        card.style.transform = "rotateY(180deg)";
+        starting = 0;
+    }
+}
+
+pic.addEventListener('click', function(){
+    console.log("clicked");
+})
+
+var item = document.getElementById("photos");
+
+window.addEventListener("wheel", function (e) {
+    if (e.deltaY > 0) pic.scrollLeft += 100;
+    else pic.scrollLeft -= 100;
 });
